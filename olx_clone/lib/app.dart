b17c6ecaf9249121de2862087d1f51e36f9f4be4
eduardo c neto <eddycn3@gmail.com>
2020-05-25 20:58:48 -0300

    olx clone init

---
+++ olx_clone/lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Container(),
+    );
+  }
+}
